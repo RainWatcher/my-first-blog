de627b60763a82b802b3270ab9a428a12b933da2
JMack <jrmac81@gmail.com>
2018-09-22 14:09:24 -0400

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,12 @@
+#This is a comment line in gray italics.
+def hi(name):
+	print('Hi ' + name + "!")
+
+girls = ['Rachel', 'Monica', 'Jessica', 'Pheobe']
+for name in girls:
+	hi(name)
+	print('Next girl')
+
+
+for i in range (7):
+	print(i)
